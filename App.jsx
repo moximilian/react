f5910f320f353a71cf64a7f6613666bae9cdf5a3
Maxim <surovmax03@gmail.com>
2023-05-04 20:22:00 +0300

    comit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 
 import "./style.css"
-import { useState } from 'react'
+import { useState } from 'node_modules/react'
 
 
 
--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'node_modules/react'
+import ReactDOM from 'node_modules/react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
