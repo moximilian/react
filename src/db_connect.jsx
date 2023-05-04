a3e0848b5fd403636209f59981db0842e70765f4
Maxim <surovmax03@gmail.com>
2023-05-04 20:11:11 +0300

    new comit

--- src/db_connect.jsx
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const db = mysql.createConnection({
-    host:"http://127.0.0.1/",
-    user:'root',
-    password:"",
-    database:'todos'
-})
-
-module.exports = db;
