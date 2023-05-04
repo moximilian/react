b93f9eb0ed371231038a3c4396e5e4e4167b800d
Maxim <surovmax03@gmail.com>
2023-05-04 21:09:05 +0300

    sorting

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 
 import "./style.css"
 import { useState } from 'react'
-
-
+import { useRef } from 'react'
+import { useEffect } from 'react'
 
 
 
@@ -10,8 +10,12 @@ import { useState } from 'react'
 export default function App() {
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
-
   
+  const [state, setState] = useState(0);
+  const renderCount = useRef(0);
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   /*function SaveTodos() {
 
@@ -27,7 +31,7 @@ export default function App() {
       return [
         ...currentTodos,
         {
-          id: crypto.randomUUID(), title: newItem, completed:
+          id: renderCount.current, title: newItem, completed:
             false
         },
       ]
@@ -36,6 +40,7 @@ export default function App() {
     setNewItem('')
   }
   function toggleTodo(id, completed) {
+
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
         if (todo.id === id) {
@@ -44,11 +49,34 @@ export default function App() {
         return todo
       })
     })
+    SortToDosById()
+    SortToDos()
+  }
+  function SortToDosById(){
+    setTodos(currentTodos =>{
+      return currentTodos.sort((a, b) => {
+        if(b.id <= a.id && a.completed===false) {
+          return 1;
+        }
+        return -1;
+      });
+    })
+  }
+  function SortToDos(){
+    setTodos(currentTodos =>{
+      return currentTodos.sort((a, b) => {
+        if(!b.completed) {
+          return 1;
+        }
+        return -1;
+      });
+    })
   }
   function deleteTodo(id) {
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
     })
+
   }
   function deleteCheckedTodos() {
     setTodos(currentTodos => {
@@ -75,7 +103,7 @@ export default function App() {
       <h1 className='header'>To Do list</h1>
       <ul className="list">
         {todos.map(todo => {
-          return <li key={todo.id}>
+          return <li key={todo.id} id = {todo.id} >
             <label>
               <input type="checkbox" checked={todo.
                 completed}
